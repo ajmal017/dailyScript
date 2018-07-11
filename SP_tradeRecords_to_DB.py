78168818d02b580f16ac66d963fbd4c2889858fa
hadrianl <137150224@qq.com>
2018-07-11 10:53:44 +0800

    保存成交数据脚本

---
+++ SP_tradeRecords_to_DB.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/9 0009 14:02
+# @Author  : Hadrianl 
+# @File    : SP_tradeRecords_to_DB.py.py
+# @License : (C) Copyright 2013-2017, 凯瑞投资
+
+import pymysql as pm
+from spapi.spAPI import *
+
+def save_trade(trade):
+    trade_dict = dict()
+    try:
+        for n, t in trade._fields_:
+            v = getattr(trade, n)
+            trade_dict[n] = v.decode() if isinstance(v, bytes) else v
+        cursor = conn.cursor()
+
+        values = ','.join(['"' + str(v) + '"' for v in trade_dict.values()])
+        sql = f'replace into sp_trade_records values({values})'
+        cursor.execute(sql)
+        conn.commit()
+        cursor.close()
+        print(f'插入数据库:{trade_dict}')
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == '__main__':
+    import configparser
+    import os
+    conf_path = os.path.dirname(__file__)
+    conf_parser = configparser.ConfigParser()
+    conf_parser.read(os.path.join(conf_path, 'conf.ini'))
+    host = conf_parser.get('SPINFO', 'host')
+    port = conf_parser.getint('SPINFO', 'port')
+    License = conf_parser.get('SPINFO', 'license')
+    app_id = conf_parser.get('SPINFO', 'appid')
+    user_id = conf_parser.get('SPINFO', 'userid')
+    initialize()
+    print('无任何消息输出时按任意键退出.....')
+    info = {'host': host, 'port': int(port), 'License': License, 'app_id': app_id, 'user_id': user_id,
+            'password': input('请输入密码：')}
+    set_login_info(**info)
+    login()
+    conn = pm.connect(host='192.168.2.226', port=3306, user='kairuitouzi', passwd='kairuitouzi',
+                      db='carry_investment')
+    conn.set_charset('utf8')
+
+
+    @on_login_reply  # 登录调用
+    def login_reply(user_id, ret_code, ret_msg):
+        if ret_code == 0:
+            print(f'<账户>{user_id.decode()}登录成功')
+        else:
+            print(f'<账户>{user_id.decode()}登录失败--errcode:{ret_code}--errmsg:{ret_msg.decode()}')
+
+
+    @on_load_trade_ready_push  # 登入后，登入前已存的成交信息推送
+    def trade_ready_push(rec_no, trade):
+        print('<成交>', f'历史成交记录--NO:{rec_no}--{trade.OpenClose.decode()}成交@{trade.ProdCode.decode()}--{trade.BuySell.decode()}--Price:{trade.AvgPrice}--Qty:{trade.Qty}')
+        save_trade(trade)
+
+    input()
+    # trades = get_all_trades_by_array()
+    # for t in trades:
+    #     save_trade(t)
+    conn.close()
+    logout()
---
+++ tradeRecords_to_DB.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/6 0006 15:53
+# @Author  : Hadrianl 
+# @File    : tradeRecords_to_DB.py
+# @License : (C) Copyright 2013-2017, 凯瑞投资
+
+"""
+python tradeRecords_to_DB.py auto_save # 启用成交回报自动保存入库
+python tradeRecords_to_DB.py save_all  # 查询成交信息，并将返回保存入库，同时启用成交回报自动保存入库
+
+"""
+
+
+from pyctp.CTPApi import CTPTrade
+import pymysql as pm
+from threading import Thread
+from pyctp.utils import struct_format, logger
+import configparser
+import os
+
+conf_path = os.path.dirname(__file__)
+conf_parser = configparser.ConfigParser()
+conf_parser.read(os.path.join(conf_path, 'conf.ini'))
+
+addr = conf_parser.get('CTPINFO', 'addr')
+brokerID = conf_parser.get('CTPINFO', 'brokerID')
+userID = conf_parser.get('CTPINFO', 'userID')
+
+db_host = conf_parser.get('MYSQL', 'host')
+db_user = conf_parser.get('MYSQL', 'user')
+db_password = conf_parser.get('MYSQL', 'password')
+db_name = conf_parser.get('MYSQL', 'db')
+
+t_api = CTPTrade()
+t_api.connect(userID, input('请输入密码:'), brokerID, addr)
+db = pm.connect(db_host, db_user, db_password, db_name, charset='utf8')
+
+@t_api.register_rtn_callback('OnRtnTrade', log_type='成交入库', log=True)
+def rtn_to_db(trade):
+    trade = struct_format(trade)
+    data = [ ('"' + v + '"' if isinstance(v, str) else str(v))for v in trade.values()]
+    cursor = db.cursor()
+    cursor.execute(f'replace into ctp_trade_records values({",".join(data)})')
+    cursor.close()
+    db.commit()
+
+@t_api.register_rsp_callback('OnRspQryTrade', log_type='成交入库', log=True)
+def rsp_to_db(trade):
+    trade = struct_format(trade)
+    data = [ ('"' + v + '"' if isinstance(v, str) else str(v))for v in trade.values()]
+    cursor = db.cursor()
+    cursor.execute(f'replace into ctp_trade_records values({",".join(data)})')
+    cursor.close()
+    db.commit()
+
+if __name__ == '__main__':
+    import sys
+    import time
+    try:
+        arg = sys.argv[1]
+    except:
+        arg = 'save_all'
+
+    if arg == 'auto_save':
+        t_thread = Thread(target=t_api.Join)
+        t_thread.start()
+    elif arg == 'save_all':
+        @t_api.register_rsp_callback('OnRspUserLogin', log_type='登入', log=True)
+        def active_qry(pRspUserLogin):
+            RspUserLogin = struct_format(pRspUserLogin)
+            logger.info(
+                f'<登入>交易接口：{RspUserLogin["UserID"]}于{RspUserLogin["LoginTime"]}登录成功,当前交易日为{RspUserLogin["TradingDay"]}')
+            t_api.qryTrade()
+        t_thread = Thread(target=t_api.Join)
+        t_thread.start()
+
+
+
+
+  # self.BrokerID = '' #经纪公司代码, char[11]
+  #       self.InvestorID = '' #投资者代码, char[13]
+  #       self.InstrumentID = '' #合约代码, char[31]
+  #       self.OrderRef = '' #报单引用, char[13]
+  #       self.UserID = '' #用户代码, char[16]
+  #       self.ExchangeID = '' #交易所代码, char[9]
+  #       self.TradeID = '' #成交编号, char[21]
+  #       self.Direction = '' #买卖方向, char
+  #       self.OrderSysID = '' #报单编号, char[21]
+  #       self.ParticipantID = '' #会员代码, char[11]
+  #       self.ClientID = '' #客户代码, char[11]
+  #       self.TradingRole = '' #交易角色, char
+  #       self.ExchangeInstID = '' #合约在交易所的代码, char[31]
+  #       self.OffsetFlag = '' #开平标志, char
+  #       self.HedgeFlag = '' #投机套保标志, char
+  #       self.Price = '' #价格, double
+  #       self.Volume = '' #数量, int
+  #       self.TradeDate = 'Date' #成交时期, char[9]
+  #       self.TradeTime = 'Time' #成交时间, char[9]
+  #       self.TradeType = '' #成交类型, char
+  #       self.PriceSource = '' #成交价来源, char
+  #       self.TraderID = '' #交易所交易员代码, char[21]
+  #       self.OrderLocalID = '' #本地报单编号, char[13]
+  #       self.ClearingPartID = 'ParticipantID' #结算会员编号, char[11]
+  #       self.BusinessUnit = '' #业务单元, char[21]
+  #       self.SequenceNo = '' #序号, int
+  #       self.TradingDay = 'Date' #交易日, char[9]
+  #       self.SettlementID = '' #结算编号, int
+  #       self.BrokerOrderSeq = 'SequenceNo' #经纪公司报单编号, int
+  #       self.TradeSource = '' #成交来源, char
